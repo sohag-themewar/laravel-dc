0966b4896b8cc323fb95934815390b0b7e84f80a
sohag-themewar <dev.11.themewar@gmail.com>
2025-03-09 09:57:59 +0600

    run app with dc

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,23 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        watch: {
+            usePolling: true,
+        },
+        hmr: {
+            host: 'localhost',
+            port: 5173,
+        },
+    },
 });
